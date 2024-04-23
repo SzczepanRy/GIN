89585e8c5372e603529d7feb0f484d3c7f303ed7
unknown <szep.rydzewski@gmail.com>
2024-04-23 23:55:50 +0200

    A

---
+++ chat/main.go
@@ -0,0 +1,34 @@
+package main
+
+/*
+
+create rooms
+
+create clients belonging to the room
+
+chose what room the client should join by asking it in the frontend (can display che choices via button)
+
+add room button??
+
+
+handle serving the static js
+
+get to know the websocket class in js
+
+as for the sobket make a imple ping pong
+
+find out how to store messages
+maby a slice of a struct ?
+
+figure out how to serve the users the right content based on waht channal the belog to
+??
+make a map of this maby??
+
+
+
+
+*/
+
+func main() {
+
+}
